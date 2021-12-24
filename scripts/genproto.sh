ec829fe1b900105bbb15a26a87700e198227139c
yankay <kay.yan@daocloud.io>
2021-12-24 10:00:54 +0800

    简化依赖

--- scripts/genproto.sh
+++ scripts/genproto.sh
@@ -7,17 +7,24 @@ set -ex
 
 export PATH=$PATH:$GOROOT/bin:$GOPATH/bin:~/go/bin/
 
-protoc -I=".:/usr/include:./internal/vender/proto" --go_out=./pkg  ./api/proto/v1/helloworld.proto
+# GRPC
+protoc -I=".:./internal/vender/proto" --go_out=./pkg  ./api/proto/v1/helloworld.proto
+protoc -I=".:./internal/vender/proto" --go-grpc_out=require_unimplemented_servers=false:./pkg  ./api/proto/v1/helloworld.proto
 
-protoc -I=".:/usr/include:./internal/vender/proto" --go-grpc_out=require_unimplemented_servers=false:./pkg  ./api/proto/v1/helloworld.proto
-protoc -I=".:/usr/include:./internal/vender/proto" --grpc-gateway_out . \
+# GRPC GW
+
+protoc -I=".:./internal/vender/proto" --grpc-gateway_out . \
        --grpc-gateway_opt logtostderr=true \
        --grpc-gateway_opt paths=source_relative \
        --grpc-gateway_opt generate_unbound_methods=true \
       ./api/proto/v1/helloworld.proto
 mv ./api/proto/v1/helloworld.pb.gw.go ./pkg/helloworld
 
+# OpenAPI
+
 protoc -I. \
-      -I=".:/usr/include:./internal/vender/proto" --openapiv2_out=. ./api/proto/v1/helloworld.proto
+      -I=".:./internal/vender/proto" --openapiv2_out=. ./api/proto/v1/helloworld.proto
 mv ./api/proto/v1/helloworld.swagger.json ./api/swagger/v1/helloworld.swagger.json
 cp ./api/swagger/v1/helloworld.swagger.json assets/swagger-ui/helloworld.swagger.json 
+
+# Typescript
